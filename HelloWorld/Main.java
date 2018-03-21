ea636806dfecca34062825a1ef89aa93368228b7
dmkonst <warqueq@gmail.com>
2018-03-21 17:03:08 +0200

    test status sonar

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,6 +1,10 @@
 package HelloWorld;
 
 public class Main
+
+  private static final String CHANNEL = "channel";
+  private static final String SOURCE = "source";
+
 {
     public static void main(String[] args)
     {

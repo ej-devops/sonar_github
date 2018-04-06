a4110c9323a08ccdae21a2230165f7f72e054612
dmkonst <warqueq@gmail.com>
2018-04-06 11:14:51 +0300

    transfer

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -2,6 +2,6 @@
       public static void main(String[] args)
       {
          System.out.println("Hello world!!!");
-         System unread = "TEST";
+         System unread = "TEST";;;
       }
 }

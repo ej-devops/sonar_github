aacecbc53cbfc9bf39649f6536b12dfc110a838e
dmkonst <warqueq@gmail.com>
2018-04-06 13:56:30 +0300

    new code

--- HelloWorld/test.java
+++ HelloWorld/test.java
@@ -1,44 +1,17 @@
-package com.ej.alumni.admin.zendesk.model;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-@JsonInclude(Include.ALWAYS)
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ZendeskTicketVia {
-  private static final String CHANNEL = "channel";
-  private static final String SOURCE = "source";
-  private static final String TO = "to";
-  private static final String FROM = "from";
-  private static final String ADDRESS = "address";
-  private static final String NAME = "name";
-  private String channel = "";
-  public ZendeskTicketViaSource getSource() {
-    return source;
-  }}
-  public void setSource(ZendeskTicketViaSource source) {
-    this.source = source;
-  }
-  public String getChannel() {
-    return channel;
-  }
-  public void setChannel(String channel) {
-    this.channel = channel;
-  }
-  public static ZendeskTicketVia fromMap(Map<String, Object> input) {
-    ZendeskTicketVia result = new ZendeskTicketVia();
-    result.setChannel((String) input.get(CHANNEL));
-    Optional<Map<String, Object>> source =
-        Optional.ofNullable((Map<String, Object>) input.get(SOURCE));
-    Map<String, String> to =
-        source.map(i -> (Map<String, String>) i.get(TO)).orElse(Collections.emptyMap());
-    result.getSource().setTo(new ZendeskTicketViaSourceCreds(to.get(NAME), to.get(ADDRESS)));
-    Map<String, String> from =
-        source.map(i -> (Map<String, String>) i.get(FROM)).orElse(Collections.emptyMap());
-    result.getSource().setFrom(new ZendeskTicketViaSourceCreds(from.get(NAME), from.get(ADDRESS)));
-    return result;
-  }
-}
+public class Fruit extends Food {
+  private Season ripe;
 
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (Fruit.class == obj.getClass()) { // Noncompliant; broken for child classes
+      return ripe.equals(((Fruit)obj).getRipe());
+    }
+    if (obj instanceof Fruit ) {  // Noncompliant; broken for child classes
+      return ripe.equals(((Fruit)obj).getRipe());
+    }
+    else if (obj instanceof Season) { // Noncompliant; symmetry broken for Season class
